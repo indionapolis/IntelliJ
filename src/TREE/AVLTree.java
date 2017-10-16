9c5815bcf8eaa4e1c0b27d1bec0099c2d3a8c405
Павел <pavel@MacBook-Pro-Pavel.local>
2017-10-16 18:28:19 +0300

    add heap

--- src/TREE/AVLTree.java
+++ src/TREE/AVLTree.java
@@ -63,7 +63,6 @@ public class AVLTree<K extends Comparable<K>, V> {
             }
         }
 
-
         balanceBranch(other);
     }
 
---
+++ src/TREE/BinaryHeap.java
@@ -0,0 +1,8 @@
+package TREE;
+
+/**
+ * Project name: HomeWork
+ * Created by pavel on 16.10.2017.
+ */
+public class BinaryHeap {
+}
