45c002cf960f7df2933281564e8f6f39f7f74c61
Павел <pavel@MacBook-Pro-Pavel.local>
2017-10-20 11:12:37 +0300

    v1.0

--- src/TREE/BinaryHeap.java
+++ src/TREE/BinaryHeap.java
@@ -1,8 +1,199 @@
 package TREE;
-
+import java.util.Arrays;
 /**
  * Project name: HomeWork
- * Created by pavel on 16.10.2017.
+ * Created by pavel on 19.10.2017.
+ * v1.0
  */
-public class BinaryHeap {
+
+public class BinaryHeap<K extends Comparable<K>, V> {
+    private static final int DEF_CAPACITY = 100;
+    private Node[] array;
+    private int size;
+
+
+    public static void main(String[] args) {
+        BinaryHeap heap = new BinaryHeap();
+        heap.insert(2, 8);
+        heap.insert(6, 8);
+        heap.insert(3, 8);
+        heap.insert(1, 8);
+        heap.insert(4, 8);
+        heap.insert(9, 8);
+        heap.insert(0, 8);
+        heap.insert(6, 8);
+        while (!heap.isEmpty()){
+            System.out.println(heap.removeMin());
+        }
+    }
+
+
+    /**
+     * Constructs a new BinaryHeap.
+     */
+    public BinaryHeap() {
+        this(DEF_CAPACITY);
+    }
+
+    public BinaryHeap(int capacity){
+        array =  new Node[capacity];
+        size = 0;
+    }
+
+
+
+    /**
+     * Add a value to the heap.
+     */
+    public void insert(K key, V value) {
+        if (isFull()) {
+            resize();
+        }
+
+        array[++size] = new Node(key, value);
+
+        goToRightPlace();
+    }
+
+
+
+    /**
+     * Returns (but does not remove) the minimum element in the heap.
+     */
+    public V min() {
+        if (isEmpty()){
+            return null;
+        }else {
+            return (V) array[1].value;
+        }
+    }
+
+
+
+    /**
+     * Removes and returns the minimum element in the heap.
+     */
+    public V removeMin() {
+        // what do want return?
+        V result = min();
+
+        // get rid of the last leaf/decrement
+        array[1] = array[size];
+        array[size--] = null;
+
+        correctPlace();
+
+        return result;
+    }
+
+
+
+
+    /**
+     * Performs operation to place the element that is at the
+     * root of the heap in its correct place so that the heap maintains the
+     * min-heap order property.
+     */
+    protected void correctPlace() {
+        int index = 1;
+
+        while (hasLeftChild(index)) {
+            // which of my children is smaller?
+            int smallerChild = leftIndex(index);
+
+            // bubble with the smaller child, if I have a smaller child
+            if (hasRightChild(index)
+                    && array[leftIndex(index)].key.compareTo(array[rightIndex(index)].key) > 0) {
+                smallerChild = rightIndex(index);
+            }
+
+            if (array[index].key.compareTo(array[smallerChild].key) > 0) {
+                swap(index, smallerChild);
+            } else {
+                // otherwise, get outta here!
+                break;
+            }
+
+            // make sure to update loop counter/index of where last el is put
+            index = smallerChild;
+        }
+    }
+
+
+    /**
+     * Performs operation to place a newly inserted element
+     * (i.e. the element that is at the size index) in its correct place so
+     * that the heap maintains the min-heap order property.
+     */
+    protected void goToRightPlace() {
+        int id = this.size;
+
+        while (hasParent(id)
+                && (parent(id).key.compareTo(array[id].key) > 0)) {
+            // parent/child are out of order; swap them
+            swap(id, parentIndex(id));
+            id = parentIndex(id);
+        }
+    }
+
+
+    protected boolean hasParent(int i) {
+        return i > 1;
+    }
+
+    protected Node parent(int i) {
+        return array[parentIndex(i)];
+    }
+
+    protected int parentIndex(int i) {
+        return i / 2;
+    }
+
+
+    protected int leftIndex(int i) {
+        return i * 2;
+    }
+
+    protected int rightIndex(int i) {
+        return i * 2 + 1;
+    }
+
+
+    protected boolean hasLeftChild(int i) {
+        return leftIndex(i) <= size;
+    }
+
+    protected boolean hasRightChild(int i) {
+        return rightIndex(i) <= size;
+    }
+
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    private boolean isFull(){
+        return size >= array.length - 1;
+    }
+
+
+    protected void resize() {
+        array = Arrays.copyOf(array, array.length * 2);
+    }
+
+
+    protected void swap(int index1, int index2) {
+        Node tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
+    }
+
+    private class Node<K extends Comparable<K>, V>{
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        K key;
+        V value;
+    }
 }
